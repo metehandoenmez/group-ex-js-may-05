c729dbfe73a70cfe845cd0a457f1c6058ac56508
adrian-ext <adrian-ext@payflows.io>
2023-05-05 09:40:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const jsonData = `{"data":[10,45,81,90,82,6,29,31,22,5,99,27,55,68,17,88,14,47,50,67]}`;
+
+function isPrime(num) {
+    if (num == 2 || num == 3) return true;
+    if (num <= 1 || num % 2 == 0 || num % 3 == 0) return false;
+    for (let i = 5; i * i <= num; i += 6)
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+    return true;
+}
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+function convert(data, actionCb) {
+    return actionCb(data);
+}
+
+const convertedData = convert(data, actionCallback);
