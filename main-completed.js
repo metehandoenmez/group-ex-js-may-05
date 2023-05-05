2b62c13732594c7242e3c68f9c390402f6a64936
adrian-ext <adrian-ext@payflows.io>
2023-05-05 10:38:18 +0300

    updated

--- main-completed.js
+++ main-completed.js
@@ -12,3 +12,86 @@ function isEven(num) {
     return num % 2 === 0;
 }
 
+function convert(data, actionCb) {
+    return actionCb(data);
+}
+
+function actionCallback(var1) {
+    let parsedData = JSON.parse(var1);
+    const { data } = parsedData;
+    return data;
+}
+
+const convertedData = convert(jsonData, actionCallback);
+
+class DataProcess {
+    constructor(data) {
+        this._data = data;
+        // this.sortData();
+    }
+
+    sortData() {
+        this._data.sort((a, b) => a - b);
+    }
+
+    calculateTotal() {
+        let totalResult = 0;
+        for (let i = 0; i < this._data.length; i++) {
+            const element = this._data[i];
+            totalResult = totalResult + element;
+        }
+        this.total = totalResult;
+    }
+    isEven() {
+        return this.total % 2 === 0;
+    }
+    doDouble() {
+        this.doubled = this._data.map((el) => el * 2);
+    }
+
+    static getOdd(dataArg1) {
+        let r = dataArg1.filter((el) => el % 2 === 1);
+        return r;
+    }
+    static getEven(dataArg1) {
+        let r = dataArg1.filter((el) => el % 2 === 0);
+        return r;
+    }
+    printProps() {
+        console.log("Data:");
+        console.log(this._data);
+
+        console.log("Doubled:");
+        console.log(this.doubled);
+
+        console.log("Total:");
+        console.log(this.total);
+    }
+    filterSmaller() {
+        this.smaller = this._data.filter((el) => el < 50);
+    }
+
+    toJSON() {
+        return {
+            data: this._data,
+            doubled: this.doubled,
+            total: this.total
+        };
+    }
+}
+
+let dataObj = new DataProcess(convertedData);
+dataObj.sortData();
+dataObj.calculateTotal();
+dataObj.doDouble();
+dataObj.filterSmaller();
+// console.log(dataObj);
+// console.log(dataObj.isEven());
+// console.log(DataProcess.getOdd(dataObj._data));
+// console.log(DataProcess.getEven(dataObj._data));
+// dataObj.printProps();
+
+console.log(JSON.stringify(dataObj));
+
+let vv = { "data": [1, 2, 3] };
+console.log(JSON.stringify(vv));
--- readme.md
+++ readme.md
@@ -1,20 +1,28 @@
-Convert jsonData object to regular js object, using a callback that returns the result from JSON.parse().
+Define a function called actionCallback, that converts jsonData object to regular js object. It returns the result from JSON.parse().
 
 Extract the list of numbers inside this object's "data" property, in a new variable.
 
 Create a class DataProcess.
 In this class:
 Create a constructor that takes this list of numbers, and stores it in this._data;
+
 Create a method called sortData which sorts all the elements in this._data, and updates this._data to be sorted.
+
 Create a method called calculateTotal, which calculates a total for this array, and saves it in this.total;
+
 Create a method called isEven, which will be true if the total is even, or false if the total is odd.
-Create a method called doubled, which doubles all the elements in our data using .map(), and stores it into this.doubled. 
+
+Create a method called doDouble, which doubles all the elements in our data using .map(), and stores it into this.doubled. 
+
 Create a static method called getOdd, which gets all the odd elements in our data, and returns them.
+
 Create a static method called getEven, which gets all the even elements in our data, and returns them.
+
 Create a method which prints all these properties we created previously, this.total, this.doubled, this._data.
+
 Create a method which filters all the elements smaller than 50, and stores them into this.smaller.
 
-Create the method toJson() which calls JSON.stringify on (this), and returns the result.
+Create the method toJSON() which returns the class properties we want to stringify, in an object like: {data: this._data}.
 Convert our ProcessData object to json, and print it.
 
 
